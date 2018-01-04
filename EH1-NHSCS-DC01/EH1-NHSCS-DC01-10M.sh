24e99513165357d34f5a61da3095adc1eef04057
ipat8 <ppatrick828@gmail.com>
2018-01-04 16:14:48 -0600

    Add SC

---
+++ EH1-NHSCS-DC01/EH1-NHSCS-DC01-10M.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Alert Script for EH1-NHSCS-DC01 - 10 Minute Intervals
+if ping -c 1 10.162.150.10 > /dev/null
+  then
+    dc01stat1=$(cat /cachet-scripts/status/eh1-nhscs-dc01-S1.txt)
+    dc01stat2=$(cat /cachet-scripts/status/eh1-nhscs-dc01-S2.txt)
+    if [ $dc01stat1 = "1" ];
+      then
+        echo 0 > /cachet-scripts/status/eh1-nhscs-dc01-S1.txt
+      else
+        :
+    if [ $dc01stat2 = "1" ];
+      then
+        echo 0 > /cachet-scripts/status/eh1-nhscs-dc01-S2.txt
+        curl -H "X-Cachet-Token: 0dKkef9ucNbQ0mGbxGkZ" -X PUT -d 'id=1&status=1' http://eh1-nhscs-cas01.ad.nhscs.net/api/v1/components/
+      else
+        :
+  else
+    echo 1 > /cachet-scripts/status/eh1-nhscs-dc01-S2.txt
+    dc01stat1=$(cat /cachet-scripts/status/eh1-nhscs-dc01-S1.txt)
+    if [ $dc01stat1 = "1" ];
+      then
+        curl -H "X-Cachet-Token: 0dKkef9ucNbQ0mGbxGkZ" -X PUT -d 'id=1&status=2' http://eh1-nhscs-cas01.ad.nhscs.net/api/v1/components/
+      else
+        exit
