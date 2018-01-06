ec5a5671691b8a8e303398d3937e96c63c4a9dec
ipat8 <ppatrick828@gmail.com>
2018-01-05 19:01:16 -0600

    Attempt to fix sintax

--- EH1-NHSCS-DC01/EH1-NHSCS-DC01-5m.sh
+++ EH1-NHSCS-DC01/EH1-NHSCS-DC01-5m.sh
@@ -9,7 +9,9 @@ if ping -c 1 10.162.150.10 > /dev/null
         curl -H "X-Cachet-Token: 0dKkef9ucNbQ0mGbxGkZ" -X PUT -d 'id=1&status=1' http://eh1-nhscs-cas01.ad.nhscs.net/api/v1/components/1
       else
         :
+    fi
   else
     echo 1 > /cachet-scripts/status/eh1-nhscs-dc01-S1.txt
     curl -H "X-Cachet-Token: 0dKkef9ucNbQ0mGbxGkZ" -X PUT -d 'id=1&status=2' http://eh1-nhscs-cas01.ad.nhscs.net/api/v1/components/1
     exit
+fi
