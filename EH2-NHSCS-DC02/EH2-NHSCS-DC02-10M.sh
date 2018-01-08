a801c1f70dc2b53476b848f611b1f0e9aba5b1b0
ipat8 <ppatrick828@gmail.com>
2018-01-08 09:27:41 -0600

    Finalise hosts

--- EH2-NHSCS-DC02/EH2-NHSCS-DC02-10M.sh
+++ EH2-NHSCS-DC02/EH2-NHSCS-DC02-10M.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-#Alert Script for EH1-NHSCS-DC01 - 10 Minute Intervals
-if ping -c 1 10.162.150.10 > /dev/null
+#Alert Script for eh2-nshcs-dc02 - 10 Minute Intervals
+if ping -c 1 10.162.150.11 > /dev/null
   then
-    dc01stat1=$(cat /cachet-scripts/status/eh1-nhscs-dc01-S1.txt)
-    dc01stat2=$(cat /cachet-scripts/status/eh1-nhscs-dc01-S2.txt)
+    dc01stat1=$(cat /cachet-scripts/status/eh2-nshcs-dc02-S1.txt)
+    dc01stat2=$(cat /cachet-scripts/status/eh2-nshcs-dc02-S2.txt)
     if [ $dc01stat1 = "1" ];
       then
-        echo 0 > /cachet-scripts/status/eh1-nhscs-dc01-S1.txt
+        echo 0 > /cachet-scripts/status/eh2-nshcs-dc02-S1.txt
       else
         :
     fi
     if [ $dc01stat2 = "1" ];
       then
-        echo 0 > /cachet-scripts/status/eh1-nhscs-dc01-S2.txt
+        echo 0 > /cachet-scripts/status/eh2-nshcs-dc02-S2.txt
         curl -H "X-Cachet-Token: 0dKkef9ucNbQ0mGbxGkZ" -X PUT -d 'id=1&status=1' http://eh1-nhscs-cas01.ad.nhscs.net/api/v1/components/
       else
         :
     fi
   else
-    echo 1 > /cachet-scripts/status/eh1-nhscs-dc01-S2.txt
-    dc01stat1=$(cat /cachet-scripts/status/eh1-nhscs-dc01-S1.txt)
+    echo 1 > /cachet-scripts/status/eh2-nshcs-dc02-S2.txt
+    dc01stat1=$(cat /cachet-scripts/status/eh2-nshcs-dc02-S1.txt)
     if [ $dc01stat1 = "1" ];
       then
         curl -H "X-Cachet-Token: 0dKkef9ucNbQ0mGbxGkZ" -X PUT -d 'id=1&status=2' http://eh1-nhscs-cas01.ad.nhscs.net/api/v1/components/
